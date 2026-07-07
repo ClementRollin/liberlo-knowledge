fa1b18bba69c502fac1d98f7ccc502221975a31f
ClementRollin1 <clemrollin01@gmail.com>
2026-07-07 19:11:22 +0200

    fix: import EmbeddingModule in ImportModule (DI resolution)

--- backend/src/import/import.module.ts
+++ backend/src/import/import.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { EmbeddingModule } from '../embedding/embedding.module';
 import { ImportController } from './import.controller';
 import { ImportService } from './import.service';
 import { GoogleDriveProvider } from './providers/google-drive.provider';
 import { ConfluenceProvider } from './providers/confluence.provider';
 
 @Module({
+  imports: [EmbeddingModule],
   controllers: [ImportController],
   providers: [ImportService, GoogleDriveProvider, ConfluenceProvider],
 })
